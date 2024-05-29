ef15d0b2ba9f4715d3b403447d8b54915d172810
Anna Crabb <annacrabb92@gmail.com>
2024-05-28 21:54:46 -0400

    new push

---
+++ people.js
@@ -0,0 +1,8 @@
+const people = ['tessa', 'emmy lou', 'crew', 'noodle'];
+const ages = [3, 3, 3, 4];
+
+console.log(people);
+
+module.exports = {
+    people, ages
+};
---
+++ test.js
@@ -0,0 +1,10 @@
+const name = "Anna";
+
+console.log(name);
+
+const greet = (name) => {
+    console.log(`hello, ${name}`);
+}
+
+greet('Tessa');
+greet('Emmy Lou');
