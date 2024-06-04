cbaa1249d1dd849b02a4ce24ccd3096c8da302ea
Anna Crabb <annacrabb92@gmail.com>
2024-06-04 14:10:23 -0400

    updates

--- app.js
+++ app.js
@@ -1,54 +1,49 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
 //express app
 const app = express();
+
 //connect to mondodb
-const dbURI = 'mongodb+srv://<newuser>:<newuser>@cluster0.sslpwey.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology:true });
+const dbURI = "mongodb+srv://annacrabb92:somethingeasy@cluster0.sslpwey.mongodb.net/"
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 //register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
-
 // middleware & static files
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-
 app.use((req, res, next) => {
-    console.log('new request made:');
-    console.log('host: ', req.hostname);
-    console.log('path: ', req.path);
-    console.log('method: ', req.method);
-    next();
-  });
-
+  res.locals.path = req.path;
+  next();
+}
+)
 app.use((req, res, next) => {
-    console.log('in the next middleware');
-    next();
-  });
+  console.log('in the next middleware');
+  next();
+});
 
+// routes
 app.get('/', (req, res) => {
-    const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-    // res.send('<p>homepage</p>');
-    res.render('index', { title: 'Home', blogs });
+ res.redirect('/blogs')
 })
+
 app.get('/about', (req, res) => {
-    // res.send('<p>aboutpage</p>');
-    res.render('about', { title: 'About'});
+  res.render('about', { title: 'About' });
 
 });
 
-app.get('/blogs/create', (req, res) =>{
-    res.render('create', { title: 'Create a Blog'});
-});
+//blog routes
+app.use('/blogs', blogRoutes);
+
 
 //404 page
 app.use((req, res) => {
-    res.statue(404).sendFile('404', { title: '404'});
-})
+  res.status(404).render('404', { title: '404' });
+}) 
